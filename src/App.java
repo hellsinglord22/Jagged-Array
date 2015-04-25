0cb0da2fda2f93132c23832f7a3b5e411d29678d
H <heshamscorpe55@gmail.com>
2015-04-25 02:10:54 +0200

    Create a description for the problem

---
+++ src/App.java
@@ -0,0 +1,9 @@
+/**
+ * Make a program to store the value of sales of each day in the year
+ * and make it easy to the programmer later to get this information
+ * and calculate average for a month or a year or two months or two
+ * years using jagged array to make the best use of the space
+ * **/
+public class App {
+
+}
